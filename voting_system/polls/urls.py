eeb2e542314ecb8af8a17521237adf1ab7663646
Marvin Kang'ethe <marvin.kangethe@andela.com>
2017-02-01 21:27:36 +0300

    Configure the app name for easy rendering.

--- voting_system/polls/urls.py
+++ voting_system/polls/urls.py
@@ -2,8 +2,11 @@ from django.conf.urls import url
 
 from . import views
 
-
+app_name = 'polls'
 urlpatterns= [
 
-	url(r'^index$',views.index, name='index')
+	url(r'^index$',views.index, name='index'),
+	url(r'^(?P<question_id>[0-9]+)/$', views.details, name='details'),
+	url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+	url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote')
 ]
