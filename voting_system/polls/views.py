d76aae18c400970b73e61190d8672719c1ef61f7
Marvin Kang'ethe <marvin.kangethe@andela.com>
2017-02-01 21:27:02 +0300

    Implemented the 404 error and render template.

--- voting_system/polls/views.py
+++ voting_system/polls/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+
+from .models import Question
 # Create your views here.
 
 
 def index(request):
-	return HttpResponse('Welcome to Andela.')
+	latest_question_list = Question.objects.all()
+	context = {'latest_question_list':latest_question_list}
+	return render(request, 'polls/index.html', context)
+
+def details(request, question_id):
+	try:
+		current_question = Question.objects.get(pk=question_id)
+	except Question.DoesNotExist:
+		raise Http404('Requested object does not exist.')
+	context={"current_question":current_question}	
+	return render(request, 'polls/details.html', context)	
+
+def results(request, question_id):
+	return HttpResponse('Results id number {}'.format(question_id))	
+
+def vote(request, question_id):
+	return HttpResponse('Vote id number {}'.format(question_id))
