0dd39d00664fe1674d01637f0f041f0cbb40c931
Marvin Kang'ethe <marvin.kangethe@andela.com>
2017-02-01 15:05:22 +0300

    beautify the return statement for the object.

--- voting_system/polls/models.py
+++ voting_system/polls/models.py
@@ -7,9 +7,15 @@ class Question(models.Model):
 	question = models.CharField(max_length=200)
 	public_date = models.DateTimeField('Date published')
 
+	def __str__(self):
+		return '{} {}'.format(self.question, self.public_date)
+
 class Choice(models.Model):
 
 	question = models.ForeignKey(Question, on_delete=models.CASCADE)
 	choice = models.CharField(max_length=200)
 	votes = models.IntegerField(default=0)
+
+	def __str__(self):
+		return '{} {}'.format(self.question.question, self.choice, self.votes)
 
